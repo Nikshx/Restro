35f16fd52cead679710cd9f571a0b6304147dc6b
nightpetal <utsavbhandari77@gmail.com>
2025-07-21 19:43:18 +0545

    commit

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,14 +1,18 @@
 from django.urls import include, path
 from .views import MenuItemList, OrderListCreateView, OrderDetailView, get_notes
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 urlpatterns = [
-    path("menu-item/", MenuItemList.as_view(), name='menu-item-list'),
-    path('menu-orders/', OrderListCreateView.as_view(),name='order-list-create'), 
-    path('menu-orders/<int:pk>/', OrderDetailView.as_view(),name='order-detail'),
-    path('notes/', get_notes),
-    
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path("menu-item/", MenuItemList.as_view(), name="menu-item-list"),
+    path("menu-orders/", OrderListCreateView.as_view(), name="order-list-create"),
+    path("menu-orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
+    path("notes/", get_notes),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path("login", views.login1, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -8,7 +8,11 @@ from .serializers import MenuItemSerializer, OrderSerializer, NoteSerializer
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout as auth_logout
 class MenuItemList(generics.ListCreateAPIView):
     serializer_class = MenuItemSerializer
 
@@ -31,3 +35,28 @@ def get_notes(request):
     notes = Note.objects.filter(owner=user)
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+
+def login1(request):
+    return render(request, "registration/login.html")
+
+# @login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('menu-item-list')
+
+# @login_required
+def profile(request):
+    return render(request, "registration/profile.html")
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_staff = False
+            user.save()
+            login(request, user)
+            return redirect("login")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
